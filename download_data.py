bbdfc2033c3b16e5328b2345fcd9b85679780075
GHZEL Ghassen <72442800+gkghzel@users.noreply.github.com>
2021-01-16 18:44:29 +0100

    download sentinel image data to "raw" folder

--- download_data.py
+++ download_data.py
@@ -1,4 +1,9 @@
 from sentinelsat.sentinel import SentinelAPI, read_geojson, geojson_to_wkt
+import os 
+
+ws = "/content/sentinel2_utility/raw"
+os.chdir(ws)
+
 api = SentinelAPI('user name goes here', 'password goes here', 'https://scihub.copernicus.eu/dhus')
 
 # download single scene by known product id
